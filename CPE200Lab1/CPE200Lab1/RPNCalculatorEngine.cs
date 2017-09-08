e4b4816959b38824cd7c20733c00e5ecf644bdab
asas2559509 <31127214+asas2559509@users.noreply.github.com>
2017-09-08 16:14:42 +0700

    เสด RPN

---
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{   //ของหลักมาจาก CalculatorEngine
+    class RPNCalculatorEngine : CalculatorEngine
+    {
+        public string Process(string str)
+        {
+            string result;
+            //Split input string to multiple parts by space
+            List<string> parts = str.Split(' ').ToList<string>();
+            Stack<string> stack = new Stack<string>();
+            for(int i = 0; i < parts.Count; i++)
+            {
+                if(parts[i]=="+"|| parts[i] == "-" || parts[i] == "X" || parts[i] == "÷" )
+                {
+                    string secondOperand = stack.Pop().ToString();
+                    string firstOperand = stack.Pop().ToString();
+                    result = calculate(parts[i], firstOperand, secondOperand, 4);
+                    stack.Push(result);
+                }
+                else
+                {
+                    stack.Push(parts[i]);
+                }
+            }
+            //stack.Push(10);
+            return stack.Pop().ToString();
+  
+        }
+    }
+}
